1e7267c8a632145b0db7da4072b917d3c3698d9f
Anil Ferhat Ceran <69785410+anilferhatceran@users.noreply.github.com>
2022-11-29 14:20:46 +0100

    Trying to fix 405 error

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,13 @@ const port = process.env.PORT || 5000
 const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const path = require('path')
+const cors = require('cors')
 
 connectDB()
 
 const app = express()
 
-
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
