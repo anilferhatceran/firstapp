abe762c0982e9cf7fb2be3d451f41e047f81cb0a
Anil Ferhat Ceran <69785410+anilferhatceran@users.noreply.github.com>
2022-11-29 14:32:13 +0100

    Trying to fix 405 error

--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { toast } from 'react-toastify'
 
-const API_URL = 'http://localhost:5000/api/goals/'
+const API_URL = '/api/goals/'
 
 // Create new goal
 const createGoal = async (goalData, token) => {
